31a90367a8908f0e145c0dedfeada9435734bee0
Eugene <eugeniy.lapshov@devexpress.com>
2019-02-14 16:08:39 +0300

    Add files via upload

---
+++ HeaderStyle/Window1.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Data;
+
+namespace HeaderStyle {
+  
+    public partial class Window1 : Window {
+        static string path = @"..\..\Nwind.xml";
+        public Window1() {
+            InitializeComponent();
+            grid.ItemsSource = GetDataFromXML();
+        }
+        private DataTable GetDataFromXML() {
+            DataSet ds = new DataSet();
+            ds.ReadXml(path);
+            return ds.Tables[0];
+        }
+    }
+   
+}
