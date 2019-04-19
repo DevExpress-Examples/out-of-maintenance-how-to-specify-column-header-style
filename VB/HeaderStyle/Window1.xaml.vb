1c8c15823ab16f67f2536e2b8e667192afa59176
DevExpressExampleBot <fakeEmail>
2019-04-19 16:34:36 +0300

    Source auto update [skip ci]

---
+++ VB/HeaderStyle/Window1.xaml.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Windows
+Imports System.Data
+
+Namespace HeaderStyle
+
+	Partial Public Class Window1
+		Inherits Window
+
+		Private Shared path As String = "..\..\Nwind.xml"
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = GetDataFromXML()
+		End Sub
+		Private Function GetDataFromXML() As DataTable
+			Dim ds As New DataSet()
+			ds.ReadXml(path)
+			Return ds.Tables(0)
+		End Function
+	End Class
+
+End Namespace
